221dc3cf92327fb5484872a2fd69f01f1f236822
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-08-10 12:36:46 -0500

    import MovieCard and replace MovieDetails

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+import MovieCard from './MovieCard';
+
 const MovieList = props => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
@@ -10,7 +12,6 @@ const MovieList = props => {
         .get('http://localhost:5000/api/movies')
         .then(response => {
           setMovies(response.data);
-          console.log(movies);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -24,30 +25,8 @@ const MovieList = props => {
     <div className="movie-list">
       {movies.map(movie => (
         <Link to={`/movies/${movie.id}`} >
-          <MovieDetails key={movie.id} movie={movie} />
+          <MovieCard key={movie.id} movie={movie} />
         </Link>
-      ))}
-    </div>
-  );
-}
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
       ))}
     </div>
   );
