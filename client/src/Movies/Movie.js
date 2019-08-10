32c93741ca9d07c26c3bc15fce66f2b4492ac065
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-08-10 12:38:36 -0500

    import MovieCard

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import MovieCard from './MovieCard';
+
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(true);
@@ -10,11 +12,11 @@ const Movie = (props) => {
     const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
+
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
-          console.log('axios.get...: response.data: ', response.data);
           setIsLoading(false);
         })
         .catch(error => {
@@ -33,25 +35,9 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
-  console.log('stars: ', stars);
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-        {!isLoading ? stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        )) : null }
-      </div>
+      {!isLoading ? <MovieCard movie={movie} /> : null}
       <div className="save-button">Save</div>
     </div>
   );
